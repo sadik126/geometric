8a672e93ec501c4eb6db6695bfc3fbcc27308982
sadik126 <sadikhimel04@gmail.com>
2024-05-07 23:18:28 +0600

    m

--- scripts/ellipse-area.js
+++
@@ -1,6 +0,0 @@
-function calculateEllipseArea(){
-    const majorRadius = getInputValueById('ellipse-major-radius');
-    const minorRadius = getInputValueById('ellipse-minor-radius');
-    const area = 3.14 * majorRadius * minorRadius;
-    setInnerTextById('ellipse-area', area);
-}
