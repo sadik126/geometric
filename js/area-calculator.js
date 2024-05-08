f65e6f2c87bac04126a3dde80b508b4f3f00b82a
sadik126 <sadikhimel04@gmail.com>
2024-05-08 21:30:59 +0600

    m

---
+++ js/area-calculator.js
@@ -0,0 +1,24 @@
+ document.getElementById('area-calculate').addEventListener('click', function(){
+
+  const baseelement = document.getElementById('base');
+  const heightelement = document.getElementById('height');
+
+  const base = parseFloat(baseelement.value) ;
+
+  const height = parseFloat(heightelement.value) ;
+
+  console.log(base , height)
+
+
+  const area = 0.5 * base * height;
+
+  console.log(area)
+
+
+  const trainglearea = document.getElementById('traingle-area')
+
+  trainglearea.innerText = area;
+
+
+
+ })
